498706e4882760ae0f6b4bdce84928b1dd426e91
Thiago Sousa Santos <thiagos.xsantos@gmail.com>
2019-12-27 14:51:37 -0300

    CHANGED: move routes to config/authentication.global.php.dist from config/module.config.php

--- config/module.config.php
+++ config/module.config.php
@@ -7,8 +7,6 @@
 namespace Zetta\ZendAuthentication;
 
 use Zend\Authentication\AuthenticationService;
-use Zend\Router\Http\Literal;
-use Zend\Router\Http\Segment;
 
 return [
     'controllers' => [
@@ -28,117 +26,6 @@ return [
         ],
         'factories' => [
             Controller\Plugin\IsAllowed::class => Controller\Plugin\Service\IsAllowedFactory::class,
-        ]
-    ],
-
-    'router' => [
-        'routes' => [
-            'authentication' => [
-                'type' => Literal::class,
-                'options' => [
-                    'route' => '/auth',
-                    'defaults' => [
-                        'controller' => 'Zetta\ZendAuthentication\Controller\Auth',
-                        'action' => 'index'
-                    ]
-                ],
-                'may_terminate' => true,
-                'child_routes' => [
-                    'authenticate' => [
-                        'type' => Literal::class,
-                        'options' => [
-                            'route' => '/authenticate',
-                            'defaults' => [
-                                'action' => 'authenticate'
-                            ]
-                        ],
-                        'priority' => 9,
-                    ],
-                    'confirm-email' => [
-                        'type' => Segment::class,
-                        'options' => [
-                            'route' => '/confirm-email/:token',
-                            'constraints' => [
-                                'token' => '[a-zA-Z0-9]*'
-                            ],
-                            'defaults' => [
-                                'action' => 'confirm-email'
-                            ]
-                        ],
-                        'priority' => 9
-                    ],
-                    'default' => [
-                        'type' => Segment::class,
-                        'options' => [
-                            'route' => '/:controller[/[:action[/[:id]]]]',
-                            'constraints' => [
-                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                                'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                                'id' => '[0-9]+'
-                            ],
-                            'defaults' => [
-                                '__NAMESPACE__' => 'Zetta\ZendAuthentication\Controller',
-                                'action' => 'index'
-                            ]
-                        ],
-                        'priority' => 5
-                    ],
-                    'password-recover' => [
-                        'type' => Segment::class,
-                        'options' => [
-                            'route' => '/password-recover/:token',
-                            'constraints' => [
-                                'token' => '[a-zA-Z0-9]*'
-                            ],
-                            'defaults' => [
-                                'action' => 'password-recover'
-                            ]
-                        ],
-                        'priority' => 9
-                    ],
-                    'recover' => [
-                        'type' => Literal::class,
-                        'options' => [
-                            'route' => '/recover',
-                            'defaults' => [
-                                'action' => 'recover'
-                            ]
-                        ],
-                        'priority' => 9
-                    ],
-                    'signin' => [
-                        'type' => Literal::class,
-                        'options' => [
-                            'route' => '/signin',
-                            'defaults' => [
-                                'action' => 'signin'
-                            ]
-                        ],
-                        'priority' => 9
-                    ],
-                    'signout' => [
-                        'type' => Literal::class,
-                        'options' => [
-                            'route' => '/signout',
-                            'defaults' => [
-                                'action' => 'signout'
-                            ],
-                        ],
-                        'priority' => 9
-                    ],
-                    'signup' => [
-                        'type' => Literal::class,
-                        'options' => [
-                            'route' => '/signup',
-                            'defaults' => [
-                                'action' => 'signup'
-                            ],
-                        ],
-                        'priority' => 9
-                    ],
-                ],
-                'priority' => 10
-            ]
         ]
     ],
 
